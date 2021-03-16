702671f68b5558094438dee893ce3d23b4cd71a0
Jacobious52 <jacobgonzalez5252@gmail.com>
2021-03-17 01:42:13 +1100

    generic

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,4 @@
-use serde::Deserialize;
-use serde_yaml::Value;
+use serde::{Deserialize, Deserializer};
 use std::collections::HashMap;
 
 pub trait Stage {
@@ -14,48 +13,56 @@ pub trait StageName {
 }
 
 #[derive(Debug, Deserialize)]
-struct StageFile {
-    stages: Vec<HashMap<String, Value>>,
+pub struct StageFile<V> {
+    stages: Vec<StageArgs<V>>,
 }
 
-type FnDeserializeStage<C> = Box<dyn Fn(Value) -> Box<dyn Stage<C = C>>>;
+#[derive(Debug, Deserialize)]
+struct StageArgs<V> {
+    name: String,
+    args: V,
+}
+
+type FnDeserializeStage<C, V> = Box<dyn Fn(V) -> Box<dyn Stage<C = C>>>;
 
 #[derive(Deserialize)]
-pub struct StageManager<C> {
+pub struct StageManager<C, V> {
     #[serde(flatten)]
-    file: StageFile,
+    file: StageFile<V>,
+
     #[serde(skip)]
-    deserialize_map: HashMap<String, FnDeserializeStage<C>>,
+    deserialize_map: HashMap<String, FnDeserializeStage<C, V>>,
 }
 
-impl<C> StageManager<C> {
-    pub fn from_str(yaml_str: &str) -> Self {
+impl<'de, C, V> StageManager<C, V>
+where
+    V: Deserialize<'de> + Deserializer<'de> + Clone,
+{
+    pub fn from_file(stage_file: StageFile<V>) -> Self {
         Self {
-            file: serde_yaml::from_str(yaml_str).unwrap(),
+            file: stage_file,
             deserialize_map: HashMap::new(),
         }
     }
 
-    pub fn run(&self, context: C) -> C {
-        let mut context = context;
-        for s in &self.file.stages {
-            s.into_iter()
-                .map(|(k, v)| {
-                    let f = &self.deserialize_map[k];
-                    let mut s = f(v.clone());
-                    s.setup();
-                    s
-                })
-                .for_each(|s| {
-                    s.run(&mut context);
-                });
-        }
-        context
+    pub fn run_stages(&self, context: &mut C) {
+        self.file
+            .stages
+            .iter()
+            .map(|s| {
+                let f = &self.deserialize_map[&s.name];
+                let mut s = f(s.args.clone());
+                s.setup();
+                s
+            })
+            .for_each(|s| {
+                s.run(context);
+            });
     }
 
     pub fn register_named<'a, S>(&mut self, name: &str) -> &mut Self
     where
-        S: 'static + Stage<C = C> + Deserialize<'a>,
+        S: 'static + Stage<C = C> + Deserialize<'de>,
     {
         self.deserialize_map.insert(
             name.to_string(),
@@ -66,14 +73,27 @@ impl<C> StageManager<C> {
 
     pub fn register<'a, S>(&mut self) -> &mut Self
     where
-        S: 'static + Stage<C = C> + StageName + Deserialize<'a>,
+        S: 'static + Stage<C = C> + StageName + Deserialize<'de>,
     {
         self.register_named::<S>(S::stage_name())
     }
 }
 
+impl<'de, C, V> Stage for StageManager<C, V>
+where
+    V: Deserialize<'de> + Deserializer<'de> + Clone,
+{
+    type C = C;
+
+    fn run(&self, c: &mut Self::C) {
+        self.run_stages(c);
+    }
+}
+
 #[cfg(test)]
 mod test {
+    use serde_yaml::Value;
+
     use super::*;
 
     #[derive(Debug, Default, Clone)]
@@ -123,19 +143,23 @@ mod test {
     fn calc_add_pipeline() {
         let yaml_str = r#"
         stages:
-        - add:
+        - name: add
+          args:
             x: 1
-        - add:
+        - name: add
+          args:
             x: 2
-        - add:
+        - name: add
+          args:
             x: 5
         "#;
 
-        let mut m = StageManager::from_str(yaml_str);
+        let file: StageFile<Value> = serde_yaml::from_str(yaml_str).unwrap();
+        let mut m = StageManager::from_file(file);
         m.register::<Add>();
 
-        let c = CalcContext { x: 1 };
-        let c = m.run(c);
+        let mut c = CalcContext { x: 1 };
+        m.run(&mut c);
 
         assert_eq!(c.x, 9);
     }
@@ -144,19 +168,23 @@ mod test {
     fn calc_mul_pipeline() {
         let yaml_str = r#"
         stages:
-        - mul:
+        - name: mul
+          args:
             x: 1
-        - mul:
+        - name: mul
+          args:
             x: 2
-        - mul:
+        - name: mul
+          args:
             x: 5
         "#;
 
-        let mut m = StageManager::from_str(yaml_str);
+        let file: StageFile<Value> = serde_yaml::from_str(yaml_str).unwrap();
+        let mut m = StageManager::from_file(file);
         m.register::<Mul>();
 
-        let c = CalcContext { x: 1 };
-        let c = m.run(c);
+        let mut c = CalcContext { x: 1 };
+        m.run(&mut c);
 
         assert_eq!(c.x, 10);
     }
@@ -165,26 +193,25 @@ mod test {
     fn calc_add_mul_pipeline() {
         let yaml_str = r#"
         stages:
-        - mul:
+        - name: mul
+          args:
             x: 1
-        - add:
+        - name: add
+          args:
             x: 2
-        # without '-' runs in random order
-          add2:
-            x: 10
-        - mul:
+        - name: mul
+          args:
             x: 5
         "#;
 
-        let mut m = StageManager::from_str(yaml_str);
-        m.register::<Mul>()
-            .register::<Add>()
-            .register_named::<Add>("add2");
+        let file: StageFile<Value> = serde_yaml::from_str(yaml_str).unwrap();
+        let mut m = StageManager::from_file(file);
+        m.register::<Mul>().register::<Add>();
 
-        let c = CalcContext { x: 1 };
-        let c = m.run(c);
+        let mut c = CalcContext { x: 1 };
+        m.run(&mut c);
 
-        assert_eq!(c.x, 65);
+        assert_eq!(c.x, 15);
     }
 
     #[test]
@@ -192,7 +219,7 @@ mod test {
         #[derive(Deserialize)]
         struct Top {
             #[serde(flatten)]
-            stages: StageManager<CalcContext>,
+            stages: StageManager<CalcContext, Value>,
         }
 
         impl Stage for Top {
@@ -203,7 +230,7 @@ mod test {
             }
 
             fn run(&self, c: &mut Self::C) {
-                *c = self.stages.run(c.clone());
+                self.stages.run(c);
             }
         }
 
@@ -215,21 +242,26 @@ mod test {
 
         let yaml_str = r#"
         stages:
-        - top:
+        - name: top 
+          args:
             stages:
-            - add:
+            - name: add
+              args:
                 x: 1
-            - add:
+            - name: add
+              args:
                 x: 4
-        - mul:
+        - name: mul
+          args:
             x: 2
         "#;
 
-        let mut m = StageManager::from_str(yaml_str);
+        let file: StageFile<Value> = serde_yaml::from_str(yaml_str).unwrap();
+        let mut m = StageManager::from_file(file);
         m.register::<Mul>().register::<Add>().register::<Top>();
 
-        let c = CalcContext { x: 1 };
-        let c = m.run(c);
+        let mut c = CalcContext { x: 1 };
+        m.run(&mut c);
 
         assert_eq!(c.x, 12);
     }
